c5121de3b2a87413862970f45ea0ba1f476f4aec
Wioleta-Kapinos <kapinos.wioleta19@gmail.com>
2023-03-26 18:58:08 +0200

    folder utils

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://pixabay.com/api/";
+const KEY = "32928385-c573e9cc533413973b7398451";
+
+const fetchImagesWithQuery = async (searchImages) => {
+    try {
+        const page = 1;
+        const response = await axios.get(`?q=${searchImages}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`);
+        return response;  
+    } 
+    catch (error) {
+        console.error(error);
+    }
+}   
+export default fetchImagesWithQuery;
